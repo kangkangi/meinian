1ff56065abf39f1e49c66a5ea1c635864931aa3c
zjk <1271311758@qq.com>
2021-12-15 20:55:44 +0800

    create ABC class

---
+++ meinian_parent/meinian_common/src/main/java/ABC.java
@@ -0,0 +1,11 @@
+package meinian_common.src.main.java;
+
+/**
+ * @author zjk
+ * @date 2021/12/15 - 20:54
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("hello ABC");
+    }
+}
